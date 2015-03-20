ca9d0a02179d145d9be46625f7b1212f10d4ef54
stephenthoen1 <sthoen16@my.whitworth.edu>
2015-03-20 09:36:10 -0700

    ForLoop test
    test

--- OSSimulationProject/OSSimulationProject/Program.cs
+++ OSSimulationProject/OSSimulationProject/Program.cs
@@ -6,7 +6,9 @@ namespace OSSimulationProject
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			for(int i = 0; i<100; i++) 
+				Console.WriteLine ("Hello World!");
+
 		}
 	}
 }
