ccc68e610e81bb2cd7243cc485d23f16cc624317
jperry1122 <jordanneperry1120@gmail.com>
2015-04-03 08:46:33 -0700

    Schedule Class & Methods added
    Schedule Class & Methods added but are blank

---
+++ SimulatorJava/src/simulatorjava/ProcessSchedules.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simulatorjava;
+
+/**
+ *
+ * @author JPerry1120
+ */
+public class ProcessSchedules {
+    public static void fifo(){
+
+ 
+    }
+    public static void rr1(){
+    
+    }
+    public static void rr10(){
+        
+    }
+    public static void shortestnext(){
+        
+    }
+    
+}
+
